3ac0d5374488084a5d5920b8a381b674e2e42236
darcyxian <darcyxianau@gmail.com>
2021-04-26 17:19:49 +1000

    first commit

---
+++ src/main/java/darcyxian/bundle_calculator/BundleCalculatorApplication.java
@@ -0,0 +1,13 @@
+package darcyxian.bundle_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundleCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BundleCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/darcyxian/bundle_calculator/BundleCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package darcyxian.bundle_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BundleCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
